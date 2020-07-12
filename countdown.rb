136d9a1843cd3571968677fc11c17296013f2119
Desiree Marquez <marquezdecardona@gmail.com>
2020-07-12 22:08:55 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -2,7 +2,7 @@
 def countdown(number_of_seconds)
   while number_of_seconds > 0 
   puts "#{number_of_seconds} SECOND(S)!"
-  number_od_seconds -= 1
+  number_of_seconds -= 1
   end
 "HAPPY NEW YEAR!"
 end
