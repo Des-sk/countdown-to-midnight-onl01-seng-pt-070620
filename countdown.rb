600754bfba1bd5503aa1c9aaf016ca65746f0fd6
Desiree Marquez <marquezdecardona@gmail.com>
2020-07-12 21:39:45 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,18 @@
-#write your code here
 
-def countdown
+def countdown(number_of_seonds)
+  while number_of_seconds > 0 
+  puts "#{number_of_seconds} SECOND(S)!"
+  number_od_seconds-=1
+end
+"HAPPY NEW YEAR!"
+end
+
+def count_down_with_sleep(number_of_seconds)
+  
+  while number_of_seconds > 0
+  puts "#{number_of_seconds} SECOND(S)!"
+  sleep(1)
+  number_of_seconds -= 1
+end
+"HAPPY NEW YEAR!"
 end
